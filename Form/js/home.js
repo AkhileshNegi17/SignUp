92853e1cd2dac2677df353788a48c65e0d752bef
Akhilesh Negi <68986968+AkhileshNegi17@users.noreply.github.com>
2020-07-30 14:11:46 +0530

    Add files via upload

---
+++ Form/js/home.js
@@ -0,0 +1,12 @@
+
+var contentpage = document.getElementById('click1');
+var contentpa = document.getElementById('click2');
+window.onclick = function(event) {
+if (event.target == contentpage) {
+contentpage.style.display = "none";
+  }
+if (event.target == contentpage) {
+contentpa.style.display = "none";
+  }
+
+}
